b53e804113b5250402a1f775eb0fa0ccd29ccfb2
Darren Tong <darrentong604@gmail.com>
2017-09-19 22:20:57 -0700

    [array] Create an Array data structure ADT exercise in JavaScript.

---
+++ data_structures/array/array.js
@@ -0,0 +1,20 @@
+/**
+ * For the given Array ADT, implement the methods and analyze time and space complexity
+ *
+ * access(index)
+ *
+ * insertAtStart(element)
+ *
+ * insertAtIndex(element, index)
+ *
+ * insertAtEnd(element)
+ *
+ * deleteAtStart()
+ *
+ * deleteAtIndex(index)
+ *
+ * deleteAtEnd()
+ *
+ * search(element)
+ *
+ **/
